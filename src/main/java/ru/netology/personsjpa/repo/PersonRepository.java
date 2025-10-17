f2d0395ef7a4f33f1ef222789763a951360aed8a
Konstantin Borshchev <wennsta@gmail.com>
2025-10-17 18:34:20 +0400

    "chore(jpa): disable open-in-view; feat(repo): mark getPersonsByCity as @Transactional(readOnly=true)"

--- src/main/java/ru/netology/personsjpa/repo/PersonRepository.java
+++ src/main/java/ru/netology/personsjpa/repo/PersonRepository.java
@@ -3,6 +3,7 @@ package ru.netology.personsjpa.repo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.netology.personsjpa.domain.Person;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class PersonRepository {
     @PersistenceContext
     private EntityManager em;
 
+    @Transactional(readOnly = true)
     public List<Person> getPersonsByCity(String city) {
         return em.createQuery(
                 "SELECT p FROM Person p WHERE UPPER(p.cityOfLiving) = UPPER(:city)",
